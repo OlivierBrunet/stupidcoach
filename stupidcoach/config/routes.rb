9e5326432871a8383fc2bc1a53acbbcd9cb71453
Olivier Brunet <mrolivierbrunet@gmail.com>
2014-08-11 15:07:26 +0200

    solved

--- stupidcoach/config/routes.rb
+++ stupidcoach/config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  get '/answer', to: 'coaching#answer'
+  get '/ask', to: 'coaching#ask', as: 'ask'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
